74fce510e41b9548cc28d72621e215f4ad7c36e8
ArthiSiva <arthisiva1008@gmail.com>
2021-05-25 11:02:30 +0530

    Initial commit OOP: Creating a Window

---
+++ src/ProcessingBallsOop.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+public class ProcessingBallsOop extends PApplet {
+
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
+    public static void main(String[] args) {
+        PApplet.main("ProcessingBallsOop",args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+}
