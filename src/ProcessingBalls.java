0cbd3a2f0224f26004e08202826f62316379ded8
ArthiSiva <arthisiva1008@gmail.com>
2021-05-25 10:15:16 +0530

    Initial Commit: Creating a window

--- src/ProcessingBalls.java
+++ src/ProcessingBalls.java
@@ -3,7 +3,6 @@ import processing.core.PApplet;
 public class ProcessingBalls extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
-    public static final int DIAMETER=10;
 
     public static void main(String[] args) {
         PApplet.main("ProcessingBalls",args);
