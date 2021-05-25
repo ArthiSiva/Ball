fd7a43addd83b6e27b6c272cfc72785b7a561975
ArthiSiva <arthisiva1008@gmail.com>
2021-05-25 10:26:36 +0530

    Creating First ball at distance of 1/5

--- src/ProcessingBalls.java
+++ src/ProcessingBalls.java
@@ -3,6 +3,7 @@ import processing.core.PApplet;
 public class ProcessingBalls extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
+    public static final int RADIUS = 10;
 
     public static void main(String[] args) {
         PApplet.main("ProcessingBalls",args);
@@ -11,7 +12,15 @@ public class ProcessingBalls extends PApplet {
     public void settings() {
         super.settings();
         size(WIDTH, HEIGHT);
-
     }
 
+    @Override
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public void draw() {
+        ellipse(0,HEIGHT*1/5, RADIUS, RADIUS);
+    }
 }
